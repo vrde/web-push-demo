a753a81fd3b91fbf74e1f00df193f799ce138745
vrde <agranzot@mailbox.org>
2020-12-19 14:35:34 +0100

    Add actual notification

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,45 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const webpush = require("web-push");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
 const port = 4000;
-
 app.get("/", (req, res) => res.send("Hello World!"));
-
 const dummyDb = { subscription: null }; //dummy in memory store
-
-const saveToDatabase = async subscription => {
+const saveToDatabase = async (subscription) => {
   // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
   // Here you should be writing your db logic to save it.
   dummyDb.subscription = subscription;
 };
-
 // The new /save-subscription endpoint
 app.post("/save-subscription", async (req, res) => {
   const subscription = req.body;
+  console.log("save subscription", subscription);
   await saveToDatabase(subscription); //Method to save the subscription to Database
   res.json({ message: "success" });
 });
-
+const vapidKeys = {
+  publicKey:
+    "BKNMV1-vx2x5t3KutZzBJnZDl5Z_2UIxhi1UfdAw8HLg2f9mJAepjlRfoCN40lqCIFcrTrOBqEsO-GJSZA7q91Q",
+  privateKey: "Fc8pn1fM_1dBw7dqpAHGrEUIHooXpT-6JEbPwOY1ZpE",
+};
+//setting our previously generated VAPID keys
+webpush.setVapidDetails(
+  "mailto:agranzot@gmail.com",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+  webpush.sendNotification(subscription, dataToSend);
+};
+//route to test send notification
+app.get("/send-notification", (req, res) => {
+  const subscription = dummyDb.subscription; //get subscription from your databse here.
+  const message = "Hello World";
+  sendNotification(subscription, message);
+  res.json({ message: "message sent" });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/service.js
+++ frontend/service.js
@@ -1,6 +1,6 @@
 // urlB64ToUint8Array is a magic function that will encode the base64 public key
 // to Array buffer which is needed by the subscription option
-const urlB64ToUint8Array = base64String => {
+const urlB64ToUint8Array = (base64String) => {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, "+")
@@ -13,14 +13,14 @@ const urlB64ToUint8Array = base64String => {
   return outputArray;
 };
 
-const saveSubscription = async subscription => {
+const saveSubscription = async (subscription) => {
   const SERVER_URL = "http://localhost:4000/save-subscription";
   const response = await fetch(SERVER_URL, {
     method: "post",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(subscription)
+    body: JSON.stringify(subscription),
   });
   return response.json();
 };
@@ -29,7 +29,7 @@ self.addEventListener("install", async () => {
   // This will be called only once when the service worker is installed for first time.
   try {
     const applicationServerKey = urlB64ToUint8Array(
-      "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk"
+      "BKNMV1-vx2x5t3KutZzBJnZDl5Z_2UIxhi1UfdAw8HLg2f9mJAepjlRfoCN40lqCIFcrTrOBqEsO-GJSZA7q91Q"
     );
     const options = { applicationServerKey, userVisibleOnly: true };
     const subscription = await self.registration.pushManager.subscribe(options);
@@ -40,10 +40,19 @@ self.addEventListener("install", async () => {
   }
 });
 
-self.addEventListener("push", function(event) {
+self.addEventListener("push", function (event) {
   if (event.data) {
     console.log("Push event!! ", event.data.text());
+    showLocalNotification("Yolo", event.data.text(), self.registration);
   } else {
     console.log("Push event but no data");
   }
 });
+
+const showLocalNotification = (title, body, swRegistration) => {
+  const options = {
+    body,
+    // here you can add more properties like icon, image, vibrate, etc.
+  };
+  swRegistration.showNotification(title, options);
+};
